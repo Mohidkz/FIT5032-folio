ccef72343708a64fe1fc13fce4df2cbb90f7fc46
Mohidkz <159775785+Mohidkz@users.noreply.github.com>
2025-10-24 09:20:46 +0000

    Deploying to gh-pages from @ Mohidkz/FIT5032-folio@23ce6071dff98d60c55957d540bc3212c8598ef4 🚀

--- src/views/AccessDeniedView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="text-center">
-    <h1>Access Denied</h1>
-    <p>You must be logged in to view this page.</p>
-    <router-link to="/login" class="btn btn-primary">Go to Login</router-link>
-  </div>
-</template>
